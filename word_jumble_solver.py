e01d15f6eec2cd01de6f739bc92741d964618c27
maybeiambatman <irtezafall2010@gmail.com>
2014-08-23 14:23:04 -0500

    Conceptual error. Should use permutations instead of substrings. Fixed

--- word_jumble_solver.py
+++ word_jumble_solver.py
@@ -2,28 +2,24 @@
 
 from sets import Set
 
-def check_word_jumble(word, dictionary):
+def check_word_jumble(word, dictionary, result_list):
     # Method that checks for all jumbled words and prints them out.
-    word_sorted = ''.join(sorted(word))
-    if word_sorted in dictionary:
-        results = dictionary[word_sorted]
+    if word in dictionary:
+        results = dictionary[word]
         for result in results:
-            print result
+            if result not in result_list:
+                result_list.append(result)
 
-def find_all_substrings(word):
-    # This method finds all the substrings (with length greater than one) of the given string
-    temp_list = []
-    for i in range (0, len(word)):
-        for j in range (i+1, len(word)+1):
-            substring = word[i:j]
-            if len(substring) > 1:
-                temp_list.append(substring)
-
-    # This returns the list of unique substrings in new_list
-    return list(Set(temp_list))
+def find_all_permuatations(word):
+    # This method finds all the permutations (with length greater than one) of the given string
+    for i in range(len(word)):
+        yield (word[i])
+        for j in find_all_permuatations(word[:i]+word[i+1:]):
+            yield(word[i]+j)
 
 def main():
     dictionary = {}
+    result_list = []
     file = open("/Users/mohdirteza/Jumbleza/words.txt", "r")
 
     print "Preparing ..."
@@ -46,9 +42,13 @@ def main():
             play = False
             break
         else:
-            substrings = find_all_substrings(input_word)
-            for substring in substrings:
-                check_word_jumble(sorted(substring), dictionary)
+            permutations = list(find_all_permuatations(input_word))
+            for permutation in permutations:
+                check_word_jumble(permutation, dictionary, result_list)
+
+        for item in result_list:
+            print item
+        print ("Total words found: %s") % (len(result_list))
 
 if __name__ == '__main__':
     main()
